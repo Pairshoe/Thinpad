1c016e51726643c41053cc112a1848f69ad12dae
roufaen <1783586258@qq.com>
2021-11-03 23:07:49 +0800

    release mode

--- thinpad_top.srcs/sources_1/new/thinpad_top.v
+++ thinpad_top.srcs/sources_1/new/thinpad_top.v
@@ -35,7 +35,7 @@ module thinpad_top(
     output wire[3:0]    ext_ram_be_n,
     output wire         ext_ram_ce_n,
     output wire         ext_ram_oe_n,
-    output wire         ext_ram_we_n,
+    output wire         ext_ram_we_n/*,
 
     // debug mode signals
     output wire[31:0]   reg_if_id_pc_now,
@@ -81,47 +81,115 @@ module thinpad_top(
     output wire[31:0]   pc,
     output wire[2:0]    time_counter,
 
-    output wire          mem_oe,
-    output wire          mem_we,
-    output wire          mem_be,
-    output wire[31:0]    mem_address,
-    output wire[31:0]    mem_data_in,
-    output wire[31:0]    mem_data_out,
+    output wire         mem_oe,
+    output wire         mem_we,
+    output wire         mem_be,
+    output wire[31:0]   mem_address,
+    output wire[31:0]   mem_data_in,
+    output wire[31:0]   mem_data_out,
 
-    output wire[31:0]    instr,
-    output wire[4:0]     reg_a,
-    output wire[4:0]     reg_b,
-    output wire[4:0]     reg_d,
-    output wire[4:0]     ins_op,
-    output wire[4:0]     ins_alu_op,
-    output wire[31:0]    imm,
-    output wire[1:0]     mem_to_reg,
-    output wire          a_select,
-    output wire          b_select,
-    output wire          pc_select,
-    output wire          mem_wr,
-    output wire          reg_wr,
+    output wire[31:0]   instr,
+    output wire[4:0]    reg_a,
+    output wire[4:0]    reg_b,
+    output wire[4:0]    reg_d,
+    output wire[4:0]    ins_op,
+    output wire[4:0]    ins_alu_op,
+    output wire[31:0]   imm,
+    output wire[1:0]    mem_to_reg,
+    output wire         a_select,
+    output wire         b_select,
+    output wire         pc_select,
+    output wire         mem_wr,
+    output wire         reg_wr,
 
-    output wire[4:0]     reg_waddr,
-    output wire[31:0]    reg_wdata,
-    output wire          reg_we,
-    output wire[4:0]     reg_raddr1,
-    output wire[4:0]     reg_raddr2,
-    output wire[31:0]    reg_rdata1,
-    output wire[31:0]    reg_rdata2,
+    output wire         br_eq,
+    output wire         br_lt,
+    output wire         br_un,
 
-    output wire[4:0]     alu_op,
-    output wire[31:0]    alu_data_a,
-    output wire[31:0]    alu_data_b,
-    output wire[31:0]    alu_data_r,
-    output wire[3:0]     alu_flag
+    output wire[4:0]    reg_waddr,
+    output wire[31:0]   reg_wdata,
+    output wire         reg_we,
+    output wire[4:0]    reg_raddr1,
+    output wire[4:0]    reg_raddr2,
+    output wire[31:0]   reg_rdata1,
+    output wire[31:0]   reg_rdata2,
+
+    output wire[4:0]    alu_op,
+    output wire[31:0]   alu_data_a,
+    output wire[31:0]   alu_data_b,
+    output wire[31:0]   alu_data_r,
+    output wire[3:0]    alu_flag*/
 );
+    // release mode signals
+    // regs between if and id
+    wire[31:0]          reg_if_id_pc_now;
+    wire[31:0]          reg_if_id_instr;
+    wire                reg_if_id_abort;
+
+    // regs between id and exe
+    wire[31:0]          reg_id_exe_pc_now;
+    wire[31:0]          reg_id_exe_data_a, reg_id_exe_data_b;
+    wire[4:0]           reg_id_exe_reg_d;
+    wire                reg_id_exe_a_select, reg_id_exe_b_select, reg_id_exe_pc_select;
+    wire[4:0]           reg_id_exe_op;
+    wire[4:0]           reg_id_exe_alu_op;
+    wire[31:0]          reg_id_exe_imm;
+    wire                reg_id_exe_mem_wr;
+    wire[1:0]           reg_id_exe_mem_to_reg;
+    wire                reg_id_exe_reg_wr;
+    wire                reg_id_exe_abort;
+
+    // regs between exe and mem
+    wire[31:0]          reg_exe_mem_pc_now;
+    wire[31:0]          reg_exe_mem_data_r, reg_exe_mem_data_b;
+    wire                reg_exe_mem_pc_select;
+    wire[4:0]           reg_exe_mem_reg_d;
+    wire[4:0]           reg_exe_mem_op;
+    wire                reg_exe_mem_mem_wr;
+    wire[1:0]           reg_exe_mem_mem_to_reg;
+    wire                reg_exe_mem_reg_wr;
+    wire                reg_exe_mem_abort;
+
+    // regs between mem and wb
+    wire[31:0]          reg_mem_wb_data;
+    wire[4:0]           reg_mem_wb_reg_d;
+    wire[4:0]           reg_mem_wb_op;
+    wire                reg_mem_wb_reg_wr;
+    wire                reg_mem_wb_abort;
+
+    wire[3:0]           stall_if, stall_id, stall_exe, stall_mem, stall_wb;
+    wire[31:0]          pc;
+    wire[2:0]           time_counter;
 
     // interface to sram and uart
-    /*wire                mem_oe, mem_we, mem_be;
+    wire                mem_oe, mem_we, mem_be;
     wire[31:0]          mem_address;
     wire[31:0]          mem_data_in;
-    wire[31:0]          mem_data_out;*/
+    wire[31:0]          mem_data_out;
+
+    // interface to decoder
+    wire[31:0]          instr;
+    wire[4:0]           reg_a, reg_b, reg_d;
+    wire[4:0]           ins_op;
+    wire[4:0]           ins_alu_op;
+    wire[31:0]          imm;
+    wire[1:0]           mem_to_reg;
+    wire                a_select, b_select, pc_select, mem_wr, reg_wr;
+
+    // interface to br_comparator
+    wire                br_eq, br_lt, br_un;
+
+    // interface to regfile
+    wire[4:0]           reg_waddr;
+    wire[31:0]          reg_wdata;
+    wire                reg_we;
+    wire[4:0]           reg_raddr1, reg_raddr2;
+    wire[31:0]          reg_rdata1, reg_rdata2;
+
+    // interface to alu
+    wire[4:0]           alu_op;
+    wire[31:0]          alu_data_a, alu_data_b, alu_data_r;
+    wire[3:0]           alu_flag;
 
     sram _sram(
         .clk            (clk_50M),
@@ -156,15 +224,6 @@ module thinpad_top(
         .uart_tsre      (uart_tsre)
     );
 
-    // interface to decoder
-    /*wire[31:0]          instr;
-    wire[4:0]           reg_a, reg_b, reg_d;
-    wire[4:0]           ins_op;
-    wire[4:0]           ins_alu_op;
-    wire[31:0]          imm;
-    wire[1:0]           mem_to_reg;
-    wire                a_select, b_select, pc_select, mem_wr, reg_wr;*/
-
     decoder _decoder(
         .inst           (instr),
         .br_eq          (br_eq),
@@ -183,9 +242,6 @@ module thinpad_top(
         .reg_wr         (reg_wr)
     );
 
-    // interface to br_comparator
-    wire                br_eq, br_lt, br_un;
-
     br_comparator _br_comparator(
         .rdata1         (reg_rdata1),
         .rdata2         (reg_rdata2),
@@ -193,13 +249,6 @@ module thinpad_top(
         .br_lt          (br_lt),
         .br_un          (br_un)
     );
-
-    // interface to regfile
-    /*wire[4:0]           reg_waddr;
-    wire[31:0]          reg_wdata;
-    wire                reg_we;
-    wire[4:0]           reg_raddr1, reg_raddr2;
-    wire[31:0]          reg_rdata1, reg_rdata2;*/
 
     regfile _regfile(
         .clk            (clk_50M),
@@ -213,11 +262,6 @@ module thinpad_top(
         .raddr2         (reg_raddr2),
         .rdata2         (reg_rdata2)
     );
-
-    // interface to alu
-    /*wire[4:0]           alu_op;
-    wire[31:0]          alu_data_a, alu_data_b, alu_data_r;
-    wire[3:0]           alu_flag;*/
 
     alu _alu(
         .op             (alu_op),
